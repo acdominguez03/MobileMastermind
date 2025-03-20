0456e070be54b1a4fd9ae28d0a80a8cb7e59657d
acdominguez03 <acordondominguez31@gmail.com>
2025-03-20 12:58:37 +0100

    Initial Commit

---
+++ MobileMastermind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileMastermind
+//
+//  Created by Andres Cordón on 20/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileMastermind/MobileMastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileMastermindApp.swift
+//  MobileMastermind
+//
+//  Created by Andres Cordón on 20/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileMastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
